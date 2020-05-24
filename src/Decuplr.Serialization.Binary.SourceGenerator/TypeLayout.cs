69a5d27f9ba855ca405b0daa8cc28e24a15bfedf
月守 Jason Kuo <jason.kuo41@gmail.com>
2020-05-25 02:14:47 +0800

    Explicit Type Info

--- src/Decuplr.Serialization.Binary.SourceGenerator/TypeLayout.cs
+++ src/Decuplr.Serialization.Binary.SourceGenerator/TypeLayout.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Text;
 using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.FindSymbols;
 
 namespace Decuplr.Serialization.Binary.SourceGenerator {
     internal struct TypeLayout {
@@ -10,6 +13,56 @@ namespace Decuplr.Serialization.Binary.SourceGenerator {
         public int OutputSize { get; set; }
         public int MinSize { get; set; }
         public int MaxSize { get; set; }
-        public IOverrideParserAttribute? OverrideParser { get; set; }
+        public ICustomParserAttribute? OverrideParser { get; set; }
+    }
+
+    // Represents an analyzed type
+    internal class TypeInfo {
+        public INamedTypeSymbol TypeSymbol { get; }
+        public List<TypeDeclarationSyntax> DeclarationSyntaxes { get; }
+        public IReadOnlyList<MemberInfo> Members { get; }
+        public List<AttributeData> Attributes { get; }
+    }
+
+    internal class MemberInfo {
+        public int Index { get; }
+        public List<MemberDeclarationSyntax> DeclarationSyntaxes { get; }
+        public IReadOnlyList<AttributeData> Attributes { get; }
+        public ISymbol MemberSymbol { get; }
+        public INamedTypeSymbol MemberTypeSymbol { get; }
+        public ICustomParserAttribute? CustomParser { get; }
+        public FormatLengthInfo? LengthInfo { get; }
+    }
+
+    internal struct FormatLengthInfo {
+        public int FixedLength { get; }
+        public int MinLength { get; }
+        public int MaxLength { get; }
+        public OverflowBehaviour OverflowBehaviour { get; }
+        public UnderflowBehaviour UnderflowBehaviour { get; }
+    }
+
+    public enum OverflowBehaviour {
+        /// <summary>
+        /// Treat the data as faulty and should stop serializing, result in a <see cref="SerializeResult.Faulted"/>
+        /// </summary>
+        FaultyData,
+
+        /// <summary>
+        /// Treat the data as normal, but trim excessive bytes
+        /// </summary>
+        TrimExcessive,
+    }
+
+    public enum UnderflowBehaviour {
+        /// <summary>
+        /// Treat the data as faulty and should stop serializing, result in a <see cref="SerializeResult.Faulted"/>
+        /// </summary>
+        FaultyData,
+
+        /// <summary>
+        /// Fill the missing data with zero bytes
+        /// </summary>
+        FillZero,
     }
 }
