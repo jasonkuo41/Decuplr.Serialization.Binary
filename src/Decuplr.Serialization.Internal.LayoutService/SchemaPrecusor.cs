e80852a238becfa97e656c36714f79f753ada519
月守 Jason Kuo <jason.kuo41@gmail.com>
2020-07-10 01:59:44 +0800

    Add external dependency member to the schema precursor

--- src/Decuplr.Serialization.Internal.LayoutService/SchemaPrecusor.cs
+++ src/Decuplr.Serialization.Internal.LayoutService/SchemaPrecusor.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using Microsoft.CodeAnalysis;
 
 namespace Decuplr.Serialization.LayoutService {
     public struct SchemaPrecusor {
 
         private IReadOnlyList<string>? _namespaces;
+
+        private IReadOnlyList<ITypeSymbol>? _externalDepedencies;
         
         public bool NeverDeserialize { get; set; }
 
@@ -15,6 +18,15 @@ namespace Decuplr.Serialization.LayoutService {
         public IReadOnlyList<string> TargetNamespaces {
             get => _namespaces ?? Array.Empty<string>();
             set => _namespaces = value;
+        }
+
+        /// <summary>
+        /// Represents the external arguments required to create this type (e.g. InlineData). 
+        /// When not empty, it cannot be a TypeParser or IParserProvider
+        /// </summary>
+        public IReadOnlyList<ITypeSymbol> ExternalDepedencies {
+            get => _externalDepedencies ?? Array.Empty<ITypeSymbol>();
+            set => _externalDepedencies = value;
         }
 
         // public InheritIndexRule InheritRule { get; set; }
